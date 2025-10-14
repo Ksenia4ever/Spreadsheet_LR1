c79958cfa37ac691d8531e0ddb1af7920b008266
Ksenia <ksenia.kyselgova@gmail.com>
2025-10-14 12:03:28 +0300

    implement datamodel (in progres)

---
+++ DataModel/Cell.cs
@@ -0,0 +1,23 @@
+﻿namespace DataModel
+{
+    public class Cell
+    {
+        public int Column { get; set; } = -1;
+        public int Row { get; set; } = -1;
+        public double Value { get; set; } = 0;
+        public string Formula { get; set; } = string.Empty;
+
+        static public Cell Empty => new Cell();
+
+        public bool IsEmpty => this.Equals(Empty);
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Cell cell &&
+                   Column == cell.Column &&
+                   Row == cell.Row &&
+                   Value == cell.Value &&
+                   Formula == cell.Formula;
+        }
+    }
+}
